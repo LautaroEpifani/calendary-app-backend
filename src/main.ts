d13e5694613d2b61efd3b1de5e79876d4c2c8507
Lautaro <epifanilautaro@gmail.com>
2024-04-08 13:56:28 +0200

    test ec2 repo changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("Cambios prueba jenkins");
+  console.log("Probando cambios para el repositorio de ec2");
   app.enableCors();
   await app.listen(3001);
 }
