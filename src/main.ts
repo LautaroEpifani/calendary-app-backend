a0e27539da562c413a0e54470b3b419908e04786
Lautaro <epifanilautaro@gmail.com>
2024-04-08 15:55:37 +0200

    test ec2 repo changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("Probando cambios para el repositorio adding github deploy keys");
+  console.log("Probando cambios para el repositorio adding github deploy keys good format");
   app.enableCors();
   await app.listen(3001);
 }
