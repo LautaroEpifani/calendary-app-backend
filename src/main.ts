e6c8d4a6ca04dc78b87b2e738f94abd21757362b
Lautaro <epifanilautaro@gmail.com>
2024-04-08 15:06:36 +0200

    test ec2 repo changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("Probando cambios para el repositorio");
+  console.log("Probando cambios para el repositorio x 4");
   app.enableCors();
   await app.listen(3001);
 }
