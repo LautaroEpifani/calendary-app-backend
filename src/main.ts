35990d152bf52b8e235d75d3a54d721b649243ea
Lautaro <epifanilautaro@gmail.com>
2024-04-19 11:22:40 +0200

    test changes instance Ec2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("Test changes inside instance EC2");
+  console.log("Test changes inside instance EC2 for video");
+ 
   app.enableCors();
   await app.listen(3001);
 }
